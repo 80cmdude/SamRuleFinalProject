fdcdf2047462244aa4813ca2060a2bf8acd41956
Samuel Rule <samuel.rule@reedonline.co.uk>
2017-09-01 12:30:38 +0100

    Added the settings package for secure variable storage on device level

---
+++ FinalProjectApp/FinalProjectApp/FinalProjectApp/Data/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectApp.Data
+{
+	public class Settings
+	{
+		private static ISettings AppSettings => CrossSettings.Current;
+
+		public static string UserName
+		{
+			get => AppSettings.GetValueOrDefault(nameof(UserName), string.Empty);
+			set => AppSettings.AddOrUpdateValue(nameof(UserName), value);
+		}
+	}
+}
