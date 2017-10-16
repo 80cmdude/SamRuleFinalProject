37c630bded4f0065146817d121799f119ef31e4f
Samuel Rule <samuel.rule@reedonline.co.uk>
2017-10-16 11:34:28 +0100

    Handle offline mode now

---
+++ FinalProjectApp/FinalProjectApp/FinalProjectApp/Alert/Alerts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectApp.Alert
+{
+	public static class Alerts
+	{
+		public async static void ServiceUnavailable()
+		{
+			await App.Current.MainPage.DisplayAlert("Service Unavailable","Please speak to a memeber of the tech team to restore the service", "Okay");
+		}
+
+		public async static void PopAlertMessage(string title, string message)
+		{
+			await App.Current.MainPage.DisplayAlert(title, message, "Okay");
+		}
+	}
+}
--- FinalProjectApp/FinalProjectApp/FinalProjectApp/Service/UserApiRequest.cs
+++ FinalProjectApp/FinalProjectApp/FinalProjectApp/Service/UserApiRequest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FinalProjectApp.Alert;
 
 namespace FinalProjectApp.Service
 {
@@ -13,17 +14,25 @@ namespace FinalProjectApp.Service
 	{
 		public async Task<bool> RegisterRequest(User user)
 		{
-			var json = await GeneratePostRequest(user);
-
-			Dictionary<string, string> newUser = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
-
-			if (newUser["success"] == "true")
+			try
 			{
-				Settings.Token = newUser["token"];
-				Settings.UserId = Convert.ToInt32(newUser["id"]);
-				return true;
+				var json = await GeneratePostRequest(user);
+				Dictionary<string, string> newUser = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+				if (newUser["success"] == "true")
+				{
+					Settings.Token = newUser["token"];
+					Settings.UserId = Convert.ToInt32(newUser["id"]);
+					return true;
+				}
+				Alerts.PopAlertMessage("User already exists", "Sorry the user entered already exists, please sign in instead");
+				return false;
 			}
-			return false;
+			catch (Exception e)
+			{
+				Alerts.ServiceUnavailable();
+				return false;
+			}
 		}
 	}
 }
