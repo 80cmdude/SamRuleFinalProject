78d05888bc25b9694999fac14d06cf535a1dcc7c
Samuel Rule <samuel.rule@reedonline.co.uk>
2017-11-15 12:01:30 +0000

    Transaction Page Added

--- FinalProjectApp/FinalProjectApp/FinalProjectApp/ViewModels/TransactionPageViewModel.cs
+++ FinalProjectApp/FinalProjectApp/FinalProjectApp/ViewModels/TransactionPageViewModel.cs
@@ -1,6 +1,8 @@
-﻿using FinalProjectApp.Service;
+﻿using FinalProjectApp.Models;
+using FinalProjectApp.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,19 @@ namespace FinalProjectApp.ViewModels
 {
 	public class TransactionPageViewModel : BaseViewModel
 	{
+		public ObservableCollection<Transaction> Transactions { get; set; }
+		
 		public async void GetTransactions()
 		{
 			TransactionApiRequest request = new TransactionApiRequest();
-			var check = await request.GetTransactions();
+			List<Transaction> transactions = await request.GetTransactions();
+			if (transactions == null)
+			{
+				//This stops the app rendering the list view when there is no data in the collection
+				Transactions = new ObservableCollection<Transaction>(new List<Transaction>());
+				return;
+			}
+			Transactions = new ObservableCollection<Transaction>(transactions);
 		}
 	}
 }
